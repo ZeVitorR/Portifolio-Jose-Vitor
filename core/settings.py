49496985f459384ba669e716457f4a760872bd13
ZeVitorR <rodriguesze6@gmail.com>
2024-12-27 16:18:34 -0300

    'atualizando'

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticPorti/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticPorti/')
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static/',
     BASE_DIR / 'portifolio/templates/static/',
