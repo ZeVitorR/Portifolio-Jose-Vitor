849b5d16b775ca915d87e307fde595705fd27b7f
José Vitor <rodriguesze6@gmail.com>
2025-01-24 18:04:23 -0300

    atualização do header

--- staticPorti/base/js/script.js
+++
@@ -1,9 +0,0 @@
-function openmenu() {
-    lista = document.getElementById('itemListaMobile')
-    if(lista.style.display == "flex"){
-        lista.style.display = "none"
-    }else{
-        lista.style.display = 'flex'
-    }
-}
-
