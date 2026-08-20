463772772c0089cdf276bd76120627c31ed43704
José Vitor Rodrigues <92634695+ZeVitorR@users.noreply.github.com>
2026-08-20 18:16:13 -0300

    Change database settings to use PostgreSQL
    Updated DATABASES configuration for PostgreSQL.

--- core/settings.py
+++ core/settings.py
@@ -81,11 +81,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': 'portifolio',
+        'USER': 'postgres',
+        'PASSWORD': '98943531',
+        'HOST': 'localhost',
+        'PORT': '5432',
 }
 
 
